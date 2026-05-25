1c89a364d388984d6713de6b85103a42c24d5fe8
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-25 03:57:47 +0300

    fix(231): verify hazard hurt dispatch

--- game/231/231_006_RB_Hazard_HurtDriver.c
+++ game/231/231_006_RB_Hazard_HurtDriver.c
@@ -1,5 +1,7 @@
 #include <common.h>
 
+// NOTE(aalhendi): ASM-verified NTSC-U 926 0x800ac1b0-0x800ac204.
+
 int DECOMP_RB_Hazard_HurtDriver(struct Driver *driverVictim, int damageType, struct Driver *driverAttacker, int reason)
 {
 	struct GameTracker *gGT = sdata->gGT;
@@ -7,9 +9,7 @@ int DECOMP_RB_Hazard_HurtDriver(struct Driver *driverVictim, int damageType, str
 
 	if ((driverVictim->actionsFlagSet & 0x100000) == 0)
 	{
-#ifndef REBUILD_PS1 // this ifndef should be removed at some point?
 		result = VehPickState_NewState(driverVictim, damageType, driverAttacker, reason);
-#endif
 	}
 	else
 	{
@@ -18,9 +18,7 @@ int DECOMP_RB_Hazard_HurtDriver(struct Driver *driverVictim, int damageType, str
 		if ((gGT->levelID == OXIDE_STATION) && (gGT->gameMode1 < 0))
 			damageType = 1;
 
-#ifndef REBUILD_PS1 // this ifndef should be removed at some point?
 		result = (int)BOTS_ChangeState(driverVictim, damageType, driverAttacker, reason);
-#endif
 	}
 	return result;
 }
